6be0d1b3ae8809486764369346033c917c8c436c
claudiopatti <claudiopatti1997@gmail.com>
2024-08-27 17:46:57 +0200

    da aggiustare non covalida bene le parole uguali

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,36 @@
-console.log('ciao console')
+
+
+
+const parolaUno = prompt('Prima parola?');
+console.log('prima parola', parolaUno, typeof parolaUno );
+
+
+const parolaDue = prompt('Prima parola?');
+console.log('prima parola', parolaDue.length, typeof parolaDue);
+
+function myArry(a,b) {
+
+
+    if (!a.lenght == b.lenght) {
+        return true;
+        console.log('sono lunghe uguali', parolaUno + parolaDue )
+        
+    }
+    else {
+        return false;
+        console.log('parola più lunga', parolaPiuLunga )
+    }
+    
+}
+
+if (myArry(parolaUno,parolaDue)) {
+    console.log('sono lunghe uguali', parolaUno + parolaDue )
+    
+} else if (parolaUno > parolaDue) {
+    console.log(parolaUno )
+    
+}
+else {
+    console.log(parolaDue )
+    
+};
